4dc32e70a87c7fc2efee0e7118593be8ae3b54ae
Ilya Baranov <mrfirebrand@icloud.com>
2022-10-18 19:25:02 +0300

    lesson_8_rest

---
+++ users/views.py
@@ -0,0 +1,13 @@
+# from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins, permissions
+# from .models import NoteUser
+from .models import User
+from .serializers import UserModelSerializer
+
+
+# class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all().order_by("username")
+    serializer_class = UserModelSerializer
